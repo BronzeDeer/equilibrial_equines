359d5d26145303c9be46a1a23ca78cbbfb8d8f32
Joel Pepper <pepper@bronze-deer.de>
2025-09-28 13:07:54 +0200

    feat: initial commit, devShell setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    # Nix supports many different os/arch combinations, but in practice we mostly care about support x86_64 and arm for linux and darwin (macOS)
+  # The list maintained by as "defaultSystems" is exactly this, i.e. ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"]
+  flake-utils.lib.eachDefaultSystem (
+    system: let
+    pkgs = import nixpkgs{
+      inherit system;
+    };
+  in {
+    devShell = pkgs.mkShell {
+      packages = with pkgs; [
+        gleam
+
+        erlang
+        elixir
+        rebar3
+      ];
+
+      shellHook = ''
+      '';
+    };
+  }
+  );
+}
